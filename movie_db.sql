cac87b2cd94f9f9bb05088eee0b3318f905a595f
aashish1814 <aashishbari456@gmail.com>
2024-08-26 16:05:22 +0545

    database modified

--- movie_db.sql
+++ movie_db.sql
@@ -72,6 +72,19 @@ CREATE TABLE `users` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `review_table`
+--
+
+CREATE TABLE `review_table` (
+  `review_id` int(11) NOT NULL,
+  `user_name` varchar(200) NOT NULL,
+  `user_rating` int(1) NOT NULL,
+  `user_review` text NOT NULL,
+  `datetime` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+--
 -- Indexes for dumped tables
 --
 
@@ -94,6 +107,13 @@ ALTER TABLE `movies`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
+
+
+--
+-- Indexes for table `review_table`
+--
+ALTER TABLE `review_table`
+  ADD PRIMARY KEY (`review_id`);
 
 
 
@@ -120,6 +140,15 @@ ALTER TABLE `movies`
 --
 ALTER TABLE `users`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+
+--
+-- AUTO_INCREMENT for table `review_table`
+--
+ALTER TABLE `review_table`
+  MODIFY `review_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+
 
 
 
