fc9ddb11d3880d41c0e833f0228c9d35f3fdde78
ashishtharu1209 <ashishk077@academiacollege.edu.np>
2024-08-26 14:49:42 +0545

    error solved

--- contact.php
+++ contact.php
@@ -45,7 +45,7 @@ if(isset($_POST['send'])){
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>contact</title>
+   <title>𝚌𝚘𝚗𝚝𝚊𝚌𝚝</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
